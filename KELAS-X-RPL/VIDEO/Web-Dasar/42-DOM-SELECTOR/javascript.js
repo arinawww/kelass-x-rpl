d41b6504274e38fa7ebff030be822debc7a385d1
arinawww <arinamaulidiyah00@gmail.com>
2025-09-06 22:37:27 +0700

    Add files via upload

---
+++ KELAS-X-RPL/VIDEO/Web-Dasar/42-DOM-SELECTOR/javascript.js
@@ -0,0 +1,5 @@
+function tampil() {
+  document.querySelector("h1").innerText = "BELAJAR JAVASCRIPT";
+  document.querySelector(".container").style.color = "red";
+  document.querySelector("#paragraf").style.fontSize = "50px";
+}
