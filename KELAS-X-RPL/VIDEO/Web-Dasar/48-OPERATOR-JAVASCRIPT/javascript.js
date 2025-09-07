0dcb7313c9af233eef8f1be48dc99fda1758b5cd
arinawww <arinamaulidiyah00@gmail.com>
2025-09-07 10:43:07 +0700

    Add files via upload

---
+++ KELAS-X-RPL/VIDEO/Web-Dasar/48-OPERATOR-JAVASCRIPT/javascript.js
@@ -0,0 +1,51 @@
+function tampil() {
+  // document.querySelector("h1").innerText = "BELAJAR JAVASCRIPT";
+  // document.querySelector(".container").style.color = "red";
+  // document.querySelector("#paragraf").style.fontSize = "50px";
+
+  // console.log(document.querySelectorAll("img"));
+
+  // min = Math.ceil(0);
+  // max = Math.floor(2);
+  // let random = Math.floor(Math.random() * (max - min + 1) + min);
+  // console.log(random);
+
+  // document.querySelectorAll("img")[random].style.visibility = "visible";
+
+
+  // alert(document.querySelector("#nama").value);
+
+  // document.querySelector("#hasil").value = document.querySelector("#nama").value;
+
+  // document.querySelector("#paragraf").innerText = "sigma";
+
+
+}
+
+btn.onclick = function () {
+
+  let a = 4
+  let b = 2
+
+  // b = b + a
+  // b = b * a
+  // b = b / a
+  b = b ** a
+  console.log(b)
+
+
+  // let menu = document.querySelector("ul")
+
+  // menu.removeChild(menu.children[0]);
+
+  // const nama = document.querySelector("#nama");
+  // const judul = document.querySelector("h1");
+
+  // let tampil = "ngoding"
+
+  // judul.innerHTML = tampil + " " + nama.value;
+
+  // tampil = "belajar"
+
+  // judul.innerHTML = nama.value;
+}
