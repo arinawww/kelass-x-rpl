c31e845ab5ce0f57a16fdd199b81fb538bc7ed2e
arinawww <arinamaulidiyah00@gmail.com>
2026-01-12 09:49:19 +0700

    Add files via upload

---
+++ KELAS-X-RPL/kelas/Tugas Pak Tri/rpl/XRPL.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package x.rpl;
+
+/**
+ *
+ * @author ME1JU
+ */
+public class XRPL {
+
+    public static void main(String[] args) {
+       int angka;
+       angka=10;
+       
+       System.out.println(angka);
+       System.out.println("angka yang anda inputkan: "+angka);
+       angka=100;
+       System.out.println("angka terbaru adalah: "+angka);
+       angka=1000;
+       System.out.println("angka yang anda inputkan: "+angka);
+       angka=10000;
+       System.out.println("angka yang anda inputkan: "+angka);
+       angka=100000;
+       System.out.println("angka yang anda inputkan: "+angka);
+       
+       final double desimal=10.355;
+       float pecahan=13.4555f;
+            System.out.println("desimal");
+            System.out.println("angka pecahan yang diinputi: "+pecahan);
+            
+       String nama= "jonathan";
+       char abjad='A';
+            System.out.println("nama");
+            System.out.println("abjad");
+            
+       boolean betul= true;
+       boolean salah= false;
+            System.out.println("betul");
+            System.out.println("salah");
+            
+       //desimal= 100.235;
+            System.out.println("desimal");
+    }
+    
+}
