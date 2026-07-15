b79f58f96422663d26d9c5e066fe662e51978e26
arinawww <arinamaulidiyah00@gmail.com>
2026-07-15 09:18:24 +0700

    Add files via upload

---
+++ XI-RPL/Pak Isa/Dasar/Namespace, Class, Main method/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BelajarCSharp // Namespace sesuai dengan nama project, bisa diubah sesuai kebutuhan
+{
+    class Program // atau sederhamamya, Class adalah tempat menuliskan program dan function
+                  // class juga dapat diibaratkan sebagai blueprint atau wadah utama program
+    {
+        static void Main(string[] args) // Main adalah method utama yang akan dijadikan pertama kali saat program dieksekusi
+        {
+            Console.WriteLine("Hello Word"); 
+        }
+    }
+}
